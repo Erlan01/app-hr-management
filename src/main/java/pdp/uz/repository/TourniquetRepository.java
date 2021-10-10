9ac7813543800747a01a0d425c92852093369b9f
erlan01 <sultamuratoverlan@gmail.com>
2021-10-10 11:50:48 +0500

    TourniquetController added

--- src/main/java/pdp/uz/repository/TourniquetRepository.java
+++ src/main/java/pdp/uz/repository/TourniquetRepository.java
@@ -4,8 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pdp.uz.entity.TourniquetCard;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface TourniquetRepository extends JpaRepository<TourniquetCard, UUID> {
+
+    Optional<TourniquetCard> findByEmployee_EmailAndStatusTrue(String email);
 }
